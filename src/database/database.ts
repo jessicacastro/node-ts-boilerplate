d22f374ffc35bc938716875bbb2701f19d7931b4
Jessica Castro <jessica.castros@outlook.com>
2023-03-16 19:08:34 -0300

    Insert global knex to understand the sqlite config

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,8 +1,17 @@
 import { knex as setupKnex } from 'knex'
 
-export const knex = setupKnex({
+export const config = {
   client: 'sqlite',
   connection: {
     filename: './tmp/database.sqlite',
   },
-})
+  migrations: {
+    directory: './src/database/migrations',
+  },
+  seeds: {
+    directory: './src/database/seeds',
+  },
+  useNullAsDefault: true,
+}
+
+export const knex = setupKnex(config)
