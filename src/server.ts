43e85165f96d755b20254506333099f1823f9ead
Jessica Castro <jessica.castros@outlook.com>
2023-03-16 18:54:18 -0300

    Insert Knex configured and with connection on sqlite3

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,13 @@ import fastify from 'fastify'
 const server = fastify()
 
 server.get('/isUp', async (request, reply) => {
-  return 'Server is running and working! You can use now!'
+  return {
+    message: 'Server is up! Start coding and thanks for use this API! 🍾',
+  }
 })
 
 server
   .listen({
     port: 3333,
   })
-  .then(() => console.log('Server is running at http://localhost:3333'))
+  .then(() => console.log('Server is running at http://localhost:3333 🚀'))
