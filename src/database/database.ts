b4cf7c2e35153eb476887eb61ae7306e5c932211
Jessica Castro <jessica.castros@outlook.com>
2023-03-16 19:12:14 -0300

    Improve Knex configs

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,14 +1,16 @@
-import { knex as setupKnex } from 'knex'
+import { knex as setupKnex, Knex } from 'knex'
 
-export const config = {
+export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
     filename: './tmp/database.sqlite',
   },
   migrations: {
+    extension: 'ts',
     directory: './src/database/migrations',
   },
   seeds: {
+    extension: 'ts',
     directory: './src/database/seeds',
   },
   useNullAsDefault: true,
